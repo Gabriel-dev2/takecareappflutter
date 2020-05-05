aa9e373da0ac3382d75bfcb0298fec6407a0f9cc
Gabriel-dev2 <gabriel23costalima@outlook.com>
2020-05-04 23:32:11 -0300

    APP-02 Arquivo de animações do app

---
+++ lib/animations/fadeAnimation.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FadeAnimation extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
